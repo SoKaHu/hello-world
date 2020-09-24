960d91f053b829bbb09a57abb6bb176b84680897
SoKaHu <71759015+SoKaHu@users.noreply.github.com>
2020-09-24 12:33:12 +0200

    Add files via upload

---
+++ Class1.java
@@ -0,0 +1,13 @@
+package hausaufgabe1;
+
+public class Class1 extends MiniJava {
+	public static void main(String[] args) {
+		int x, y, result;
+		x=readInt("geben Sie x ein");
+		y=readInt("geben Sie y ein");
+		result= x + y;
+		write(result);
+		
+	}
+
+}
